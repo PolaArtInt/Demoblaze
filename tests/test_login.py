ad7bb3a8008b1f9e57abfb427ed5d8205d736074
Pola Goruho <merrula.crimson@gmail.com>
2025-04-10 20:54:04 +0400

    added .yml, pytest.ini

--- tests/test_login.py
+++ tests/test_login.py
@@ -1,19 +1,30 @@
 import pytest
+import allure
 
 
-# DEMO
+@allure.feature('Авторизация')
+@allure.title('Авторизаиця с недействительными учетными данными')
 def test_login_failure(login_page):
-    login_page.navigate()
-    login_page.login('user', 'password')
+    with allure.step('Открыть страницу авторизации'):
+        login_page.navigate()
+    with allure.step('Ввести в форму авторизации недействительные учетные данные'):
+        login_page.login('invalid_user', 'invalid_password')
+    with allure.step('Отображается ошибка - Invalid credentials. Please try again.'):
+        assert login_page.get_error_message() == 'Invalid credentials. Please try again.'
 
-    assert login_page.get_error_message() == 'Invalid credentials. Please try again.'
 
-
+@allure.feature('Login')
+@allure.story('Login with valid credentials')
+@allure.severity(allure.severity_level.CRITICAL)
+@allure.title('Авторизаиця с корректными учетными данными')
 @pytest.mark.parametrize('username, password', [
     ('user', 'user'),
     ('admin', 'admin')
 ])
 def test_login_success(login_page, dashboard_page, username, password):
-    login_page.navigate()
-    login_page.login(username, password)
-    dashboard_page.assert_welcome_message(f"Welcome {username}")
+    with allure.step('Открыть страницу авторизации'):
+        login_page.navigate()
+    with allure.step('Ввести в форму авторизации недействительные учетные данные'):
+        login_page.login(username, password)
+    with allure.step('Отображается приветственное сообщение с именем пользователя'):
+        dashboard_page.assert_welcome_message(f"Welcome {username}")
