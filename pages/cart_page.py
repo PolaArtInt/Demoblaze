d857f210e442c4305b31d1ad201ec17502ce3a71
Pola Goruho <merrula.crimson@gmail.com>
2025-05-09 01:15:44 +0400

    automated DBLZ-6 Cart Page (without authorization) - Check the page elements are present on the page

--- pages/cart_page.py
+++ pages/cart_page.py
@@ -8,3 +8,7 @@ class CartPage(BasePage):
 
     def __init__(self, page: Page):
         super().__init__(page)
+        self.CART_PAGE_TITLE = page.locator('(//h2)[1]')
+        self.CART_PAGE_TOTAL = page.locator('(//h2)[2]')
+        self.CART_PAGE_TABLE = page.locator('//table')
+        self.PLACE_ORDER_BUTTON = page.locator('//button[@data-target="#orderModal"]')
