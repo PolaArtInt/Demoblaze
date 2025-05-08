4416bba6fd7346b73be629c63754f1be82771870
Pola Goruho <54287155+PolaArtInt@users.noreply.github.com>
2025-05-09 01:31:06 +0400

    Fixed typo (#8)
    * automated DBLZ-6 Cart Page (without authorization) - Check the page elements are present on the page
    
    * fixed typo

--- tests/test_login_page.py
+++ tests/test_login_page.py
@@ -2,7 +2,7 @@ from pages.login_page import *
 
 
 class TestLoginPage:
-    def test_login(self, cart_page, login_page):
+    def test_login(self, login_page):
         login_page.visit()
         login_page.login("Pola2", "123")
         expect(login_page.LOGOUT_LINK).to_be_visible()
