432a9309f7cc03d95d2d3fb8c640a211f7a2ec50
Pola Goruho <54287155+PolaArtInt@users.noreply.github.com>
2025-05-09 01:12:00 +0400

    added login function to login_page, tested login, commented all the demo (#6)

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,3 +1,5 @@
+import pytest
+import allure
 from playwright.sync_api import Page, expect
 
 from base.base_page import BasePage
@@ -8,3 +10,17 @@ class LoginPage(BasePage):
 
     def __init__(self, page: Page):
         super().__init__(page)
+        self.LOGIN_LINK = page.locator('//a[@id="login2"]')
+        self.LOGOUT_LINK = page.locator('//a[@id="logout2"]')
+        self.WELCOME_TEXT = page.locator('//a[@id="nameofuser"]"]')
+
+        self.USERNAME_INPUT = page.locator('//input[@id="loginusername"]')
+        self.PASSWORD_INPUT = page.locator('//input[@id="loginpassword"]')
+        self.LOGIN_BTN = page.locator('//button[@onclick="logIn()"]')
+
+    @allure.step("User login")
+    def login(self, username, password):
+        self.LOGIN_LINK.click()
+        self.USERNAME_INPUT.fill(username)
+        self.PASSWORD_INPUT.fill(password)
+        self.LOGIN_BTN.click()
