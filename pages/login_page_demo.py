7c1f37da13ff953b4df1e34c0ce37e611358c3ed
Pola Goruho <merrula.crimson@gmail.com>
2025-05-09 01:08:40 +0400

    added login function to login_page, tested login, commented all the demo

--- pages/login_page_demo.py
+++ pages/login_page_demo.py
@@ -1,28 +1,28 @@
-from playwright.sync_api import Page
-
-from base.base_page import BasePage
-
-
-# DEMO
-class LoginPageDemo(BasePage):
-    def __init__(self, page: Page):
-        super().__init__(page)
-        self.page = page
-        self.username_input = page.locator('#username')
-        self.password_input = page.locator('#password')
-        self.login_button = page.locator('#login')
-        self.error_message = page.locator('#errorAlert')
-
-    def navigate(self):
-        """Открывает страницу логина."""
-        self.page.goto('https://zimaev.github.io/pom/')
-
-    def login(self, username: str, password: str):
-        """Выполняет вход с заданными учетными данными."""
-        self.username_input.fill(username)
-        self.password_input.fill(password)
-        self.login_button.click()
-
-    def get_error_message(self):
-        """Возвращает текст сообщения об ошибке."""
-        return self.error_message.inner_text()
+# from playwright.sync_api import Page
+#
+# from base.base_page import BasePage
+#
+#
+# # DEMO
+# class LoginPageDemo(BasePage):
+#     def __init__(self, page: Page):
+#         super().__init__(page)
+#         self.page = page
+#         self.username_input = page.locator('#username')
+#         self.password_input = page.locator('#password')
+#         self.login_button = page.locator('#login')
+#         self.error_message = page.locator('#errorAlert')
+#
+#     def navigate(self):
+#         """Открывает страницу логина."""
+#         self.page.goto('https://zimaev.github.io/pom/')
+#
+#     def login(self, username: str, password: str):
+#         """Выполняет вход с заданными учетными данными."""
+#         self.username_input.fill(username)
+#         self.password_input.fill(password)
+#         self.login_button.click()
+#
+#     def get_error_message(self):
+#         """Возвращает текст сообщения об ошибке."""
+#         return self.error_message.inner_text()
---
+++ tests/test_login_page.py
@@ -0,0 +1,8 @@
+from pages.login_page import *
+
+
+class TestLoginPage:
+    def test_login(self, cart_page, login_page):
+        login_page.visit()
+        login_page.login("Pola2", "123")
+        expect(login_page.LOGOUT_LINK).to_be_visible()
